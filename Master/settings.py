ca562883de6a097833088e85f91f073942615e6e
Vinicius Feld <vinifeld26@wgmail.com>
2024-08-21 01:35:50 -0300

    CHANGE: Insert app in static root

--- Master/settings.py
+++ Master/settings.py
@@ -135,7 +135,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Localização dos arquivos estáticos
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, 'RemoveBG/staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "RemoveBG/static")]
 
 LOGIN_REDIRECT_URL = '/admin/'
