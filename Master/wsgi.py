9d689d8a99d8da680babc0a9b23f29e015490c7a
Vinicius Feld <vinifeld26@wgmail.com>
2024-09-02 00:25:46 -0300

    CHANGE: wsgi

--- Master/wsgi.py
+++ Master/wsgi.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.insert(0, '/home/masterremovebg/apps_wsgi/Remove-Master-BG')
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Master.settings')
 
 from django.core.wsgi import get_wsgi_application
