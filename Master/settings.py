cceb58b8c5e8e80da78220c5cfe6034848a56fd7
Vinicius Feld <vinifeld26@wgmail.com>
2024-09-04 00:56:49 -0300

    CREATE: Trusted origins

--- Master/settings.py
+++ Master/settings.py
@@ -111,6 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CSRF_TRUSTED_ORIGINS = [os.getenv('ALLOWED_HOSTS').split(',')]
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
